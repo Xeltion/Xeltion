fa2f44639e23a393555b3eace8cc573f256f106a
Niskarsh <50167941+Xeltion@users.noreply.github.com>
2022-10-19 23:37:28 +0530

    Create 005_Reacting_To_User's_Score.cpp
    I tried my hand at the switch function. At first when I was presented with the problem, I tried to brute-force it using if-else statements. 
    Then I searched online and found out there's something called a switch(), which is new to me. So it was fun trying it out.

---
+++ 005_Reacting_To_User's_Score.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	char Grade;
+	puts("What di you score in today's test? : \n");
+	scanf("%c", &Grade);
+	switch(Grade)
+	{
+		case 'A':
+			printf("You did Excellent!!!");
+			break;
+		case 'B':
+			printf("You did great!");
+			break;
+		case 'C':
+			printf("You can do better.");
+			break;
+		case 'D':
+			printf("Please prepare well for the next time.");
+			break;
+		case 'F':
+			printf("You've failed the test.");
+			break;
+	}
+	
+	return 0;	
+}
