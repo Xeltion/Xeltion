841f6aa644379728d2e5d1527b59ef9758d08b37
Niskarsh <50167941+Xeltion@users.noreply.github.com>
2022-10-20 17:07:36 +0530

    Create 007_Guessing_Game.cpp
    Basic game created using while looping mechanism

---
+++ 007_Guessing_Game.cpp
@@ -0,0 +1,19 @@
+// Building a Guessing Game
+#include<stdio.h>
+int main(){
+	int Secret_Num=62;
+	int Guess;
+	
+	printf("WELCOME TO THE GUESSING GAME!!! \n\nThe Rules are simple, Guess the number to win!!! \n");
+	printf("Choose a Number :\n");
+	scanf("%d", &Guess);
+	
+	while(Guess!=Secret_Num){
+		printf("TOO BAD!!! TRY AGAIN! \n");
+		break;
+	}
+	if(Guess==Secret_Num){
+		printf("CONGRATULATIONS!!! YOU WIN!!!");
+	}
+	return 0;
+}
