cc63d1845ba48959db09b24b44053d598126c53b
Niskarsh <50167941+Xeltion@users.noreply.github.com>
2022-10-22 11:43:31 +0530

    Create 008_Basic_Pointers.py

---
+++ 008_Basic_Pointers.py
@@ -0,0 +1,23 @@
+//Pointers
+#include<stdio.h>
+int main()
+{
+	int Num;
+	int *pNum = &Num;
+	char Name[30];
+	char *pName = &Name[30];
+	const double Pi = 3.14;
+	const double *pPi = &Pi;
+	
+	puts("Enter a Number : ");
+	scanf("%d", &Num);
+	printf("Enter your Name: ");
+	scanf("%s", &Name);
+
+	printf("\nHere's The Memory Adress of the following Data: \n");
+	printf("1. Number = %p\n", pNum);
+	printf("2. Name = %p\n", pName);
+	printf("3. Constant Pi = %p", pPi);
+
+	return 0;
+}
