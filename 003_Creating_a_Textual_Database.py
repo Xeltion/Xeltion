1a568f5289f8b625581b50f87ddd7f9643d56e2a
Niskarsh <50167941+Xeltion@users.noreply.github.com>
2022-10-23 19:44:34 +0530

    Create 003_Creating_a_Textual_Database.py

---
+++ 003_Creating_a_Textual_Database.py
@@ -0,0 +1,53 @@
+#Creating a short DATABASE
+from datetime import datetime
+#call for input of some kind
+#store it in database
+print("")
+print("WELCOME TO THE XELTION DATABASES!\n")
+print("You can establish the No. of Entries the database will have, and can modify them later.")
+print("")
+print("------------------------------------------------------------------------------------------------------------------")
+print("")
+i=1
+
+Ans=input("Do you want to write a New Entry? (Y/N): ")
+while Ans=="Y" or Ans=="y":
+    if i==1:
+        print("")
+        print("Entry No. ", i)
+        print("")
+        Name = input("Name:")
+        Id = input("Identification No. :")
+        time = datetime.now()
+        print("Details of Entry :", time)
+        file = open("XeltionData.txt", "w")
+        L = [Name, Id, time]
+        rec ="RECORD: \n" + str(L[0]) + "  |  " + str(L[1]) + "  |  " + str(L[2])
+        file.write(rec)
+        file.close()
+        print("")
+        Ans = input("Do you want to add something? (Y/N):")
+        if Ans=="Y" or"y":
+            i=i+1
+        else:
+            print("INVALID INPUT!")
+            break
+    else:
+        print("")
+        print("Entry No. ", i)
+        print("")
+        Name = input("Name:")
+        Id = input("Identification No. :")
+        time = datetime.now()
+        print("Details of Entry :", time)
+        file = open("XeltionData.txt", "a")
+        L = [Name, Id, time]
+        rec ="\n" + str(L[0]) + "  |  " + str(L[1]) + "  |  " + str(L[2])
+        file.write(rec)
+        file.close()
+        print("")
+        Ans = input("Do you want to add something? (Y/N):")
+        if Ans=="Y" or"y":
+            i=i+1
+        else:
+            break
