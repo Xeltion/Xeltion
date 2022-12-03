1f269297d068761907ed624f1ab3189c9591b25f
Niskarsh <50167941+Xeltion@users.noreply.github.com>
2022-12-03 13:48:40 +0530

    010_Global_&_Local_Var.c

---
+++ 010_Global_&_Local_Var.c
@@ -0,0 +1,11 @@
+/* Global and Local Variables */
+#include <stdio.h>
+
+int			global_var=20;
+
+int main(){
+	int 	local_var=10;
+	printf("\n Local Variable [%d]", local_var);
+	printf("\n Global Variable [%d]", global_var);
+	return 0;
+}
