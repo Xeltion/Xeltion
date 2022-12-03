991aa5ece509d4070c5dfef2c887cfc6a10cb413
Niskarsh <50167941+Xeltion@users.noreply.github.com>
2022-12-03 14:04:46 +0530

    006_Sum(n).c

---
+++ 006_Sum(n).c
@@ -0,0 +1,30 @@
+/* print a series 1+2+3+4... upto n digits 
+----------------------------------------------------------------------------------------------------------------------------
+Expected Output:
+
+Please specify the number of terms you want to compute till:
+10
+
+1 + 2 + 3 + 4 + ... + 10 == 55
+
+----------------------------------------------------------------------------------------------------------------------------
+
+*/
+
+#include <stdio.h>
+
+void main(){
+	int 		Num_of_terms,index = 0;
+	int 		S = 0;
+	
+	printf("Please specify the number of terms you want to compute till :\n");
+	scanf("%d", &Num_of_terms);
+	
+	while(index<Num_of_terms){
+		//Numbers are added, but I don't want to show it everytime. I will print the final result outside this loop.
+		index++;
+		S = S+index;
+	}
+	
+	printf("1 + 2 + 3 + 4 + ... + %d == %d", Num_of_terms, S);
+}
