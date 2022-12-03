a5ecd3365292fd7acce152ffaec874588d5dc168
Niskarsh <50167941+Xeltion@users.noreply.github.com>
2022-12-03 14:08:49 +0530

    Update 008_Basic_Pointers.c

--- 008_Basic_Pointers.c
+++ 008_Basic_Pointers.c
@@ -9,11 +9,13 @@ void main(){
 	const double 		Pi = 3.14;
 	const double 		*pPi = &Pi;
 	
+	/* Input from the USER */
 	puts("Enter a Number : ");
 	scanf("%d", &Num);
 	printf("Enter your Name: ");
 	scanf("%s", &Name);
-
+	
+	/* Output */
 	printf("\nHere's The Memory Adress of the following Data: \n");
 	printf("1. Number = %p\n", pNum);
 	printf("2. Name = %p\n", pName);
