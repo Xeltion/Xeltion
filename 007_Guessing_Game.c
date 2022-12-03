bc707dc10b4dd75f454b96c473b8f375c43dd3ab
Niskarsh <50167941+Xeltion@users.noreply.github.com>
2022-12-03 14:06:31 +0530

    007_Guessing_Game.c

--- 007_Guessing_Game.c
+++ 007_Guessing_Game.c
@@ -1,8 +1,10 @@
-// Building a Guessing Game
-#include<stdio.h>
+/* Building a Guessing Game */
+#include <stdio.h>
+
 int main(){
-	int Secret_Num=62, GuessCount=1, GuessLimit=3;
-	int Guess;
+	int 		Secret_Num=62, GuessCount=1, GuessLimit=3;
+	int 		Guess;
+	
 	printf("WELCOME TO THE GUESSING GAME!!! \n\nThe Rules are simple, Guess the number to win!!! \n");
 	printf("You have only 3 Lives, So think caefully about your number.\n");
 	printf("Choose a Number :\n");
@@ -13,7 +15,7 @@ int main(){
 			printf("TOO BAD!!! TRY AGAIN! \n");
 			GuessCount++;
 			scanf("%d", &Guess);
-		} else {
+		} else{
 			break;
 		}
 	}
