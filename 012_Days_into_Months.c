34e639a6bb9b93f2878edf8da11542772a4ba19a
Niskarsh <50167941+Xeltion@users.noreply.github.com>
2022-12-03 14:21:52 +0530

    012_Days_into_Months.c

---
+++ 012_Days_into_Months.c
@@ -0,0 +1,15 @@
+/* Days --> Months */
+
+#include <stdio.h>
+
+int main(){
+	int 			No_of_Days, Months, Days;
+	printf("Enter the number of days: \n");
+	scanf("%d", &No_of_Days);
+	
+	Months = No_of_Days / 30;
+	Days = No_of_Days % Months;
+	
+	printf("Months: [%d], Days: [%d]", Months, Days);
+	return 0;
+}
