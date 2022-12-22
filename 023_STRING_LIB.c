ea9a192646ae4153f8f784d91b5d5cc7d82b8fb6
Niskarsh <50167941+Xeltion@users.noreply.github.com>
2022-12-23 00:46:49 +0530

    Update 023_STRING_LIB.c

--- 023_STRING_LIB.c
+++ 023_STRING_LIB.c
@@ -7,5 +7,7 @@
 *	(isalnum, isalpha, isdigit, isblank, isnull, isspace); 
 *	(strcat, strncat); 
 *	(strchr, strstr) --> Searching Functions ;
+* strrev --> Reverses a string
+* (strupr, strlwr) 
 *----------------------------------------------------------------------------------------------
 */
