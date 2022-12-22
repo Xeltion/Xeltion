2d8bc2db2a45992e75e1da4a71b2db6a5b719098
Niskarsh <50167941+Xeltion@users.noreply.github.com>
2022-12-23 01:13:00 +0530

    HELPING NOTE

---
+++ 025_MATHS_LIB.c
@@ -0,0 +1,44 @@
+/* MATHS FUNCTION 
+*--------------------------------------------------------------------------------------------------------------
+*	MACROS:-
+*	
+*	Exception Handling:
+*	HUGE_VALF, HUGE_VALL
+*	math_errhandling {{{ MATH_ERRNO + MATH_ERREXCEPT }}}
+*	NAN
+*	
+*	Comparing Macros:
+*	[int isgreater(__,__) , int isgreaterequal(__,__)]
+*	[int isless(__,__) , int islessequal(__,__)]
+*	islessgreater(__,__) --> a!=b
+*	int isunordered(__,__)
+*
+*	Classifying Macros:
+*	int isfinite(__)
+*	int isinf(__)
+*	int isnormal(__)
+*	int isnan(__)
+*	
+*	FUNCTIONS:-
+*	
+*	pow(__,__) , sqrt(__) , cbrt(__) , exp(__,__)		-->		___float(f) and long double(l)
+*	[ceil(__), floor(__)] 								--> 	___float floorf(), long double floorl(); float ceilf(), long double ceill()____
+*	remainder(__) 										--> 	___float remainderf(), long double remainderl()____
+*	fabs(__) 											--> 	Return only the magnitude of the input.
+*	fmax(__,__) , fmin(__,__)
+*
+*	Trignometry Functions:
+*	cos() , cosh() , acos() , acosh()
+*	sin() , sinh() , asin() , asinh()
+*	tan() , tanh() , atan() , atanh()
+*	
+*	Logarithmic Functions:
+*	log(__) 											--> 	Natural Log
+*	log10(__) 											--> 	log to the base 10
+*	log2(__) 											--> 	log to the base 2
+*	
+*
+*-------------------------------------------------------------------------------------------------------------
+*
+*/
+
